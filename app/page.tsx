266872de5210e78a7d2e90b3b62add3f9e323404
Temagic <temagic85@gmail.com>
2024-08-02 20:31:41 +0300

    check italy branch

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-yellow-500 text-center"><p className="text-red-800">Hello</p></div>
+    <div className="bg-yellow-500 text-center"><p className="text-green-800">Hello</p></div>
   );
 }
